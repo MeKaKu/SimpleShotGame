f500fd422568a94437a8554d88542176ba449f19
diker <OUJIANJAPAN@163.com>
2022-01-17 11:22:45 +0800

    完善地图

--- SimpleShotGame/Assets/Editor/MapEditor.cs
+++ SimpleShotGame/Assets/Editor/MapEditor.cs
@@ -6,8 +6,13 @@ using UnityEditor;
 [CustomEditor(typeof(MapGenerator))]//编辑器针对的对象
 public class MapEditor : Editor{ //扩展编辑器
     public override void OnInspectorGUI(){
-        base.OnInspectorGUI();
+        //base.OnInspectorGUI();
         MapGenerator map = target as MapGenerator;
-        map.GenerateMap();
+        if(DrawDefaultInspector()){//属性栏的值改变
+            map.GenerateMap();
+        }
+        if(GUILayout.Button("Generate Map")){
+            map.GenerateMap();
+        }
     }
 }
--- SimpleShotGame/Assets/Scripts/MapGenerator.cs
+++ SimpleShotGame/Assets/Scripts/MapGenerator.cs
@@ -6,19 +6,25 @@ public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;//地板的预制体
     public Transform obstaclePrefab;
-    public Vector2 mapSize;//地图大小（n*m）
+    public float scale = 1; //地图缩放比例
     [Range(0,1)]
     public float outlinePercent;//地板外轮廓大小
-    [Range(0,1)]
-    public float obstaclePercent;
-    public int seed = 10;
+    public Transform navmeshFloor;//导航网格
+    public Vector2 navSize;//导航区域的大小
+    //public Transform navmeshMasker;//空气墙
     List<Coord> tileCoords;
     Queue<Coord> randomTileCoords;
-    Coord birthCoord;
+    public Map[] maps;//地图数组
+    public int mapIndex;
+    Map currentMap;
     private void Start() {
         GenerateMap();
     }
     public void GenerateMap(){
+        currentMap = maps[mapIndex];//当前生成的地图
+        System.Random rand = new System.Random(currentMap.seed);//获取一个随机数序列
+        GetComponent<BoxCollider>().size = new Vector3(currentMap.mapSize.x * scale, .05f, currentMap.mapSize.y * scale);
+
         string mapHolderName = "Generated Map";//地图存放的文件夹的名字
         if(transform.Find(mapHolderName)){//已经存在地图
             DestroyImmediate(transform.Find(mapHolderName).gameObject);//删除旧的地图
@@ -27,38 +33,46 @@ public class MapGenerator : MonoBehaviour
         Transform mapHolder = new GameObject(mapHolderName).transform;//新建地图存放的文件夹
         mapHolder.parent = transform;
 
-        for(int x=0;x<mapSize.x;x++){
-            for(int y=0;y<mapSize.y;y++){
+        //生成地图地板
+        for(int x=0;x<currentMap.mapSize.x;x++){
+            for(int y=0;y<currentMap.mapSize.y;y++){
                 Vector3 tilePosition = CoordToPos(x,y);//地板位置
                 Transform newTile = Instantiate(tilePrefab,tilePosition,Quaternion.Euler(Vector3.right*90)) as Transform;
-                newTile.localScale = Vector3.one*(1-outlinePercent);//地板大小
+                newTile.localScale = Vector3.one*(1-outlinePercent) * scale;//地板大小
                 newTile.parent = mapHolder;
             }
         }
 
         //生成障碍物
-        birthCoord = new Coord((int)mapSize.x/2,(int)mapSize.y/2);//出生点
         tileCoords = new List<Coord>();//坐标列表
-        for(int x = 0; x < mapSize.x; x++){
-            for(int y = 0; y < mapSize.y; y++){
+        for(int x = 0; x < currentMap.mapSize.x; x++){
+            for(int y = 0; y < currentMap.mapSize.y; y++){
                 tileCoords.Add(new Coord(x,y));
             }
         }
-        randomTileCoords = new Queue<Coord>(Utility.ShuffleArray<Coord>(tileCoords.ToArray(), seed));//打乱的坐标列表
+        randomTileCoords = new Queue<Coord>(Utility.ShuffleArray<Coord>(tileCoords.ToArray(), currentMap.seed));//打乱的坐标列表
 
-        bool[,] obstacles = new bool[(int)mapSize.x,(int)mapSize.y];//标记每一个位置是否放置了障碍物
+        bool[,] obstacles = new bool[(int)currentMap.mapSize.x,(int)currentMap.mapSize.y];//标记每一个位置是否放置了障碍物
         int obstaclePlaced = 0;
-        int obstacleCount = (int)(mapSize.x * mapSize.y * obstaclePercent);//墙（障碍物）数目
+        int obstacleCount = (int)(currentMap.mapSize.x * currentMap.mapSize.y * currentMap.obstaclePercent);//墙（障碍物）数目
         for(int i = 0;i < obstacleCount;i++){
             Coord randomCoord = GetRandomCoord();
-            if(randomCoord!=birthCoord){
+            if(randomCoord!=currentMap.birthCoord){
                 obstacles[randomCoord.x,randomCoord.y] = true;//在该位置试放障碍物
                 obstaclePlaced++;
                 if(MapIsOneConnected(obstacles, obstaclePlaced)){//地图是单联通的
                     //放置障碍物
-                    Vector3 obstaclePos = CoordToPos(randomCoord.x, randomCoord.y) + Vector3.up * .5f;
+                    float obstacleHeight = Mathf.Lerp(currentMap.minObstacleHeight,currentMap.maxObstacleHeight,(float)rand.NextDouble());//障碍物的高度
+                    Vector3 obstaclePos = CoordToPos(randomCoord.x, randomCoord.y) + Vector3.up * obstacleHeight/2;
                     Transform newObstacle = Instantiate(obstaclePrefab,obstaclePos,Quaternion.identity) as Transform;
+                    newObstacle.localScale = new Vector3((1-outlinePercent) * scale, obstacleHeight, (1-outlinePercent) * scale);//障碍物大小
                     newObstacle.parent = mapHolder;
+
+                    Renderer obstacleRenderer = newObstacle.GetComponent<Renderer>();
+                    Material obstacleMaterial = new Material(obstacleRenderer.sharedMaterial);
+                    float colorPercent = randomCoord.y / (float)currentMap.mapSize.y;
+                    obstacleMaterial.color = Color.Lerp(currentMap.foregroundColor,currentMap.backgroundColor,colorPercent);
+                    obstacleRenderer.sharedMaterial = obstacleMaterial;
                 }
                 else{
                     obstacles[randomCoord.x,randomCoord.y] = false;
@@ -66,6 +80,10 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
+
+        //导航网格
+        navSize = new Vector2(currentMap.mapSize.x, currentMap.mapSize.y);
+        navmeshFloor.localScale = new Vector3(navSize.x,navSize.y) * scale;
     }
     //判断一个网格图是否是单联通的
     bool MapIsOneConnected(bool[,] obstacles, int obstaclePlaced){
@@ -75,8 +93,8 @@ public class MapGenerator : MonoBehaviour
         int[] dy = new int[4]{0,-1,1,0};
         int connectedSize = 1;
         Queue<Coord> q = new Queue<Coord>();
-        q.Enqueue(birthCoord);
-        vis[birthCoord.x,birthCoord.y] = true;
+        q.Enqueue(currentMap.birthCoord);
+        vis[currentMap.birthCoord.x,currentMap.birthCoord.y] = true;
         while(q.Count>0){
             Coord u = q.Dequeue();
             for(int i=0;i<4;i++){
@@ -89,17 +107,18 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
-        return connectedSize + obstaclePlaced == mapSize.x * mapSize.y;
+        return connectedSize + obstaclePlaced == currentMap.mapSize.x * currentMap.mapSize.y;
     }
     //判断一个坐标是否在网格图内
     bool InGrid(int x,int y){
-        return x >=0 && y >= 0 && x < mapSize.x && y < mapSize.y;
+        return x >=0 && y >= 0 && x < currentMap.mapSize.x && y < currentMap.mapSize.y;
     }
     Coord GetRandomCoord(){//获取随机坐标
         Coord randomCoord = randomTileCoords.Dequeue();//从随机序列里面获取
         randomTileCoords.Enqueue(randomCoord);
         return randomCoord;
     }
+    [System.Serializable]
     public struct Coord{//坐标结构体
         public int x,y;
         public Coord(int _x,int _y){
@@ -115,6 +134,25 @@ public class MapGenerator : MonoBehaviour
     }
 
     Vector3 CoordToPos(int x,int y){
-        return new Vector3(-mapSize.x/2 + x + (mapSize.x%2==0?.4f:0), 0, -mapSize.y/2 + y + (mapSize.y%2==0?.5f:0));
+        return new Vector3(-currentMap.mapSize.x/2 + x + (currentMap.mapSize.x%2==0?.5f:0), 0, -currentMap.mapSize.y/2 + y + (currentMap.mapSize.y%2==0?.5f:0)) * scale;
+    }
+
+    //地图类，存放地图的属性
+    [System.Serializable]
+    public class Map{
+        public Coord mapSize;//地图大小（n*m）
+        [Range(0,1)]
+        public float obstaclePercent;//障碍物数目
+        public int seed;//随机种子
+        public float minObstacleHeight;//障碍物最小高度
+        public float maxObstacleHeight;//障碍物最大高度
+        public Color foregroundColor;//前景颜色
+        public Color backgroundColor;//后景颜色
+
+        public Coord birthCoord {
+            get{
+                return new Coord(mapSize.x/2, mapSize.y/2);
+            }
+        }
     }
 }
