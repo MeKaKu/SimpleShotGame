424e9abef431edc3e2cd35f4a8ca4fa4284c2a77
diker <OUJIANJAPAN@163.com>
2022-01-18 22:42:59 +0800

    简单的游戏结束UI
    Restart Button

--- SimpleShotGame/Assets/Scripts/GameUI.cs
+++ SimpleShotGame/Assets/Scripts/GameUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
@@ -29,5 +30,10 @@ public class GameUI : MonoBehaviour
             percent += Time.deltaTime * _speed;
             yield return null;
         }
+    }
+
+    //UI Input
+    public void StartNewGame(){
+        SceneManager.LoadScene("SampleScene");
     }
 }
